949f045168e223a70d60109e4ae60fb4990e8f2b
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-10-11 15:00:20 -0700

    Update lang-transpile-perms.js

--- lang-transpile-perms.js
+++ lang-transpile-perms.js
@@ -1,1 +1,26 @@
 
+
+let perms = [];
+
+array.forEach((ele0)=>{
+
+  array.forEach((ele1)=>{
+  
+      if (ele0 !== ele1) perms.push({ from: ele0, to: ele1 });
+    
+  });
+  
+});
+
+
+
+
+let url = `https://github.com/search?q=`;
+
+perms.forEach((obj)=>{
+  
+  const {from, to} = obj; 
+  
+  console.log(`${url}transpile+${from}+to+${to}`);
+  
+})
