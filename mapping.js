b9279c773ac56510d0a16d695e5c88d7ff42e8bc
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-10-11 14:50:44 -0700

    Update mapping.js

--- mapping.js
+++ mapping.js
@@ -1,1 +1,51 @@
+module.exports = [
+  {
+      from: "javascript",
+      to:   "python",
+      libraries: [
+          `https://github.com/kevinbarabash/js2py.git`,
+          `https://github.com/Chad-Mowbray/js_to_py_transpiler.git`,
+          `https://github.com/swirly/pythonexec.js.git`
+      ]
+  },
+  {
+      from: "",
+      to:   "",
+      libraries: [
+          
+      ]
+  },
+
+  {
+      from: "",
+      to:   "",
+      libraries: [
+          
+      ]
+  },
+  {
+      from: "",
+      to:   "",
+      libraries: [
+          
+      ]
+  },
+  {
+      from: "",
+      to:   "",
+      libraries: [
+          
+      ]
+  },
+  {
+      from: "",
+      to:   "",
+      libraries: [
+          
+      ]
+  }
+];
+
+
+
 
