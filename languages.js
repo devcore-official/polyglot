d00378f9e98f01219aaa6c4b7ff4da2eb63ef3d5
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-10-11 15:05:30 -0700

    Update languages.js

--- languages.js
+++ languages.js
@@ -1,1 +1,34 @@
+module.exports = [
+    "bash",
+    "powershell",
+    "VBA",
+  
+    "javascript",
+    "nodejs",
+    "python",
+    "php",
+    "ruby",
+    "perl",
+    "lua",
+    "r",
+    "lisp",
 
+    "kotlin",
+
+    "swift",
+    "go",
+    "java",
+    
+    "c++",
+
+    "c",
+    "rust",
+    "fortran",
+
+    "x86",
+    "arm32",
+    "arm64",
+
+    "web assembly",
+  
+  ]
